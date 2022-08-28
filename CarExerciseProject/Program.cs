966bc2f3df1e34469bec90f58210b4cecd735344
Matteo DeSimone <thematteod@gmail.com>
2022-08-28 15:00:44 -0400

    Initial commit

---
+++ CarExerciseProject/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CarExerciseProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var personalCar = new Car
+            {
+                Make = "Infiniti",
+                Model = "Q50",
+                Year = 2015
+            };
+
+            Console.WriteLine($"{personalCar.Year} {personalCar.Make} {personalCar.Model}");
+        }
+    }
+}
